962143e08350d828d97213b438cb20563ee44d93
gpoulos02 <gpoulos2@uwo.ca>
2024-01-24 18:40:01 -0500

    initial main page

--- livinglearningapp/src/App.js
+++ livinglearningapp/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function App() {
+  // Handler for logout
+  const handleLogout = () => {
+    console.log('Logout action');
+    // Implement your logout logic here
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+        <Container>
+          <Navbar.Brand href="#home">
+            {/* You can place a logo here using an img tag if you have one */}
+            MyApp
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link1">Link 1</Nav.Link>
+              <Nav.Link href="#link2">Link 2</Nav.Link>
+              {/* ... other links ... */}
+            </Nav>
+            <Nav>
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Your page content goes here */}
     </div>
   );
 }
