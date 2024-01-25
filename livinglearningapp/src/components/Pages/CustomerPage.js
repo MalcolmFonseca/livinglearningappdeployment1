5919c939709533b38cdf578c2b9027614c4491db
gpoulos02 <gpoulos2@uwo.ca>
2024-01-24 19:33:44 -0500

    routing to different user actors home pages

---
+++ livinglearningapp/src/components/Pages/CustomerPage.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+
+function CustomerPage() {
+
+
+    const handleLogout = () => {
+        console.log('Logout action');
+        // Implement your logout logic here
+      };
+    
+      return (
+        <div className="App">
+          <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+            <Container>
+              <Navbar.Brand href="#home">
+                {/* You can place a logo here using an img tag if you have one */}
+                Living and Learning
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="#about">ABOUT US</Nav.Link>
+                  <Nav.Link href="#games">GAMES</Nav.Link>
+                  <Nav.Link href="#chat">CHAT</Nav.Link>
+                  <Nav.Link href="#resources">RESOUCES</Nav.Link>
+                  <Nav.Link href="#events">EVENTS</Nav.Link>
+                  {/* ... other links ... */}
+                </Nav>
+                <Nav>
+                  <Nav.Link onClick={handleLogout}>LOGOUT</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+    
+          {/* Your page content goes here */}
+        </div>
+      );
+}
+
+export default CustomerPage;
---
+++ livinglearningapp/src/components/Pages/EmployeePage.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+
+
+function EmployeePage() {
+
+
+    const handleLogout = () => {
+        console.log('Logout action');
+        // Implement your logout logic here
+      };
+    
+      return (
+        <div className="App">
+          <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+            <Container>
+              <Navbar.Brand href="#home">
+                {/* You can place a logo here using an img tag if you have one */}
+                Living and Learning
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="#payment">PAYMENT</Nav.Link>
+                  <Nav.Link href="#schedule">SCHEDULE</Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Link onClick={handleLogout}>LOGOUT</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+    
+          {/* Your page content goes here */}
+        </div>
+      );
+}
+
+export default EmployeePage;
