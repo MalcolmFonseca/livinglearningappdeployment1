22bd4cfb0533cfc307aeae380f7db14453b67409
gpoulos02 <gpoulos2@uwo.ca>
2024-01-31 18:17:44 -0500

    changing main directory

--- livinglearningapp/src/pages/About.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navbar, Nav, Container,NavDropdown } from 'react-bootstrap';
-
-function About(){
-    return(
-    <div className="About Section"></div>
-    )
-}
-export default About
