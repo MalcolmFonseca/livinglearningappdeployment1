4f295a19f0d33b5466db60512ef477cdc06565b5
dzivano2 <dzivano2@uwo.ca>
2024-01-31 19:41:17 -0500

    updating ui

--- livinglearningapp/src/components/Pages/About/AboutPage.js
+++ livinglearningapp/src/components/Pages/About/AboutPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Container,NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, Container,NavDropdown,Row,Col,Image } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import logo from './logo.png';
 import './About.css'
@@ -32,18 +32,22 @@ function About(){
     
           {/* Your page content goes here */}
         </div>
-        <div className="about">
-            <div className="text">
-                <h>About Us</h>
-                <p>
-                    Ongoing Living and learning is a OLLI, a registered not-for-profit caregiver-driven company dedicated to fostering a community of
-                    inclusion and forming a circle of friendship that enriches the lives of our loved ones with intellectual disabilities, while also 
-                    providing support for the entire family.
-                    Our commitment revolves around four key areas of focus: Cheer Group, Cheer Works, Cheer Connections, and Cheer Living.
-                </p>
-            </div>
-            <div className="logo"><img src={logo}/></div>
-        </div>
+        <Container className="about-section my-5">
+      <Row className="align-items-center">
+        <Col md={7} className="text-section">
+          <h2>About Us</h2> {/* Use h2 for section headings */}
+          <p>
+            Ongoing Living and learning is an OLLI, a registered not-for-profit caregiver-driven company dedicated to fostering a community of
+            inclusion and forming a circle of friendship that enriches the lives of our loved ones with intellectual disabilities, while also
+            providing support for the entire family. Our commitment revolves around four key areas of focus: Cheer Group, Cheer Works, Cheer
+            Connections, and Cheer Living.
+          </p>
+        </Col>
+        <Col md={5} className="image-section text-center">
+          <Image src={logo} alt="Company Logo" fluid /> {/* Use the Image component for responsive images */}
+        </Col>
+      </Row>
+    </Container>
     </div>
     )
 }
